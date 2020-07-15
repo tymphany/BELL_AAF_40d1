84985c7dcb6206b583f7713336ed8308df33267a
pj.hong <pj.hong@tymphany.com>
2020-07-15 19:48:56 +0800

    add Qualcomm patch for GAIA disconnect

--- adk/src/topologies/tws/common/procedures/tws_topology_procedure_disconnect_handset.c
+++ adk/src/topologies/tws/common/procedures/tws_topology_procedure_disconnect_handset.c
@@ -20,6 +20,9 @@
 #include <logging.h>
 
 #include <message.h>
+#ifdef ENABLE_TYM_PLATFORM /*add Qualcomm patch*/
+#include <gaia_framework.h>
+#endif
 
 static void twsTopology_ProcDisconnectHandsetHandleMessage(Task task, MessageId id, Message message);
 void TwsTopology_ProcedureDisconnectHandsetStart(Task result_task,
@@ -65,7 +68,9 @@ void TwsTopology_ProcedureDisconnectHandsetStart(Task result_task,
     DEBUG_LOG("TwsTopology_ProcedureDisconnectHandsetStart");
 
     appDeviceGetHandsetBdAddr(&handset_addr);
-
+#ifdef ENABLE_TYM_PLATFORM /*add Qualcomm patch*/
+	gaiaFrameworkInternal_GaiaDisconnect();
+#endif
     /* Request to Handset Services to disonnect Handset even it is disonnected,
     Handset Services sends HANDSET_SERVICE_DISCONNECT_CFM if nothing to do, this
     message is used by topology to send TWS_TOPOLOGY_HANDSET_DISCONNECTED_IND to
