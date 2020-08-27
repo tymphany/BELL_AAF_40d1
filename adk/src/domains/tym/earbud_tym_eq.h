6d7cfc7a597319881876296de78f8e318f6cb577
pj.hong <pj.hong@tymphany.com>
2020-08-27 18:38:55 +0800

    ambient/speech level Reversed

--- adk/src/domains/tym/earbud_tym_eq.h
+++ adk/src/domains/tym/earbud_tym_eq.h
@@ -4,7 +4,8 @@
 /*20200612 modify ambient/speech filter */
 /*20200617 modify ambient/speech filter */
 /*20200724 modify user eq*/
-#define EQ_VER   "0.0.0.5"
+/*20200827 level Reversed */
+#define EQ_VER   "0.0.0.6"
 
 
 #endif
