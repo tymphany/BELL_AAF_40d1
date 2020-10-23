b5c7a956313b0ad36772ac704c03f1ce51fd87b0
pj.hong <pj.hong@tymphany.com>
2020-10-23 17:34:27 +0800

    update EQ version and set ProfileEnable=true

--- adk/src/domains/tym/earbud_tym_eq.h
+++ adk/src/domains/tym/earbud_tym_eq.h
@@ -5,7 +5,8 @@
 /*20200617 modify ambient/speech filter */
 /*20200724 modify user eq*/
 /*20200827 level Reversed */
-#define EQ_VER   "0.0.0.6"
+/*20201023 update SPK EQ and add headromm for scratch noise*/
+#define EQ_VER   "0.0.0.7"
 
 
 #endif
